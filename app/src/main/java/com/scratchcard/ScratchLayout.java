716a6d811af037d382656c9c54887c6f5cf280d0
Nikhil Lohar <nikhillohar2297@gmail.com>
2020-03-06 10:22:37 +0530

    attr file added

--- app/src/main/java/com/scratchcard/ScratchLayout.java
+++ app/src/main/java/com/scratchcard/ScratchLayout.java
@@ -1,6 +1,7 @@
 package com.scratchcard;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,14 +9,21 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class ScratchLayout extends ConstraintLayout {
+    private static final int SCRATCH_TYPE_COLOR = 1;
+    private static final int SCRATCH_TYPE_DRAWABLE = 2;
+
     private Bitmap bitmap;
 
     private Paint erasePaint;
@@ -30,31 +38,79 @@ public class ScratchLayout extends ConstraintLayout {
 
     private int checkCount;
 
+    private Context context;
+
     private int[] pixels;
+
+    //ScratchCard Layout Attr
+    private int scratchRef;
+    private int scratchType;
+    private int eraseStrokeWidth;
+    private int revealPercent;
+
+    //ScratchCard Layout Default Attr
+    private int defScratchRef = Color.BLUE;
+    private int defScratchType = SCRATCH_TYPE_COLOR;
+    private int defEraseStrokeWidth = 40;
+    private int defRevealPercent = 40;
 
     private CustomListener customListener;
 
     public ScratchLayout(Context context) {
         super(context);
-        setWillNotDraw(false);
+
+        this.context = context;
+
+        init(null);
     }
 
     public ScratchLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setWillNotDraw(false);
+
+        this.context = context;
+
+        init(attrs);
     }
 
     public ScratchLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setWillNotDraw(false);
+
+        this.context = context;
+
+        init(attrs);
+
     }
 
+    private void init(AttributeSet set){
+        setWillNotDraw(false);
+
+        if (set != null) {
+            TypedArray ta = context.obtainStyledAttributes(set, R.styleable.ScratchLayout);
+            scratchType = ta.getInt(R.styleable.ScratchLayout_scratch_type, defScratchType);
+
+            if (scratchType == SCRATCH_TYPE_COLOR){
+                scratchRef = ta.getColor(R.styleable.ScratchLayout_scratch_on, defScratchRef);
+            }else if (scratchType == SCRATCH_TYPE_DRAWABLE){
+                scratchRef = ta.getResourceId(R.styleable.ScratchLayout_scratch_on, defScratchRef);
+            }
+
+            eraseStrokeWidth = ta.getInt(R.styleable.ScratchLayout_erase_stroke_width, defEraseStrokeWidth);
+            revealPercent = ta.getInt(R.styleable.ScratchLayout_reveal_percent, defRevealPercent);
+
+            ta.recycle();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onDrawForeground(Canvas canvas) {
+       // secondCanvas.setBitmap();
         canvas.drawBitmap(bitmap, 0, 0, outerPaint);
+
         super.onDrawForeground(canvas);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 
@@ -72,8 +128,19 @@ public class ScratchLayout extends ConstraintLayout {
         if (secondCanvas == null) {
             bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
             secondCanvas = new Canvas(bitmap);
-            secondCanvas.drawColor(Color.BLUE);
+
+            Rect rectBound = secondCanvas.getClipBounds();
+
+            if (scratchType == SCRATCH_TYPE_DRAWABLE){
+                Drawable customImage = getResources().getDrawable(scratchRef);
+                customImage.setBounds(rectBound);
+                customImage.draw(secondCanvas);
+            }else if (scratchType == SCRATCH_TYPE_COLOR){
+                secondCanvas.drawColor(scratchRef);
+            }
         }
+
+
 
         if (drawnPath == null) {
             drawnPath = new Path();
@@ -157,7 +224,7 @@ public class ScratchLayout extends ConstraintLayout {
 
                 float percent = (((float) transparentTotal) / ((area))) * 100;
                 Log.d("CustomLayout : ", String.valueOf(percent));
-                if (percent >= 40) {
+                if (percent >= revealPercent) {
                     bitmap.eraseColor(Color.TRANSPARENT);
                     secondCanvas.drawColor(Color.TRANSPARENT);
 
